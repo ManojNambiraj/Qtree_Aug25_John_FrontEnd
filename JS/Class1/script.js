f94582e107caac58e9d8dfe0045763deae0aec52
ManojNambiraj <postboxmano@gmail.com>
2025-09-18 21:03:46 +0530

    JS Intro

---
+++ JS/Class1/script.js
@@ -0,0 +1,60 @@
+// Functionality:
+    // Built-in functions
+    // User defined functions
+
+// alert("Good evening")
+// console.log("Good evening");
+
+// Javascript
+
+    // ES5
+    // ES6 --> EcmaScript
+
+// Variable Declarations:
+
+    // var
+
+        // var a = 10
+
+        // var a = 20
+
+        // console.log(a);
+        
+    // let
+
+        // let a = 10
+
+        // console.log(a);
+
+        // a = 20
+
+        // console.log(a);
+        
+    // const
+
+        // const a = 10;
+
+        // console.log(a);
+    
+// Scoping
+
+    // Global scope
+
+        // {
+        //    var a = 10;
+        
+        //    console.log("inside--> ", a);
+        
+        // }
+
+        // console.log("outside--> ", a);
+
+    // Local scope
+
+        // {
+        //     const a = 10;
+
+        //     console.log("inside--> ", a);
+        // }
+
+        // console.log("outside--> ", a);
