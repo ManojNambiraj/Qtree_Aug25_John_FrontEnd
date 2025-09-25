732b77e94b5ce10dd80831a3bb49af286c0d73a6
ManojNambiraj <postboxmano@gmail.com>
2025-09-25 20:30:09 +0530

    Control statements

---
+++ JS/Class2/script.js
@@ -0,0 +1,11 @@
+// Datatypes: 
+
+    // let a = true
+
+    // console.log(a, typeof(a));
+
+// Variables:
+
+let my_age_in_my_school_days = 20
+
+console.log(my_age_in_my_school_days);
---
+++ JS/Class3/script.js
@@ -0,0 +1,81 @@
+// Operators:
+
+    // Arithmetic Operators
+        // (+, -, *, /, %, **)
+
+        // let a = 5
+        // let b = 3
+        // let result
+
+        // // result = a + b
+        // // result = a - b
+        // // result = a * b
+        // // result = a / b
+        // // result = a % b
+        // result = a ** b
+
+        // console.log(result);
+
+    // Assignment Operators
+        // (=, +=, -=, *=, /=, %=, **=)
+
+        // let a = 5
+
+        // // a += 2    // a = a + 2
+        // a -= 2    // a = a - 2
+
+        // console.log(a);
+        
+    // Comparison Operators  --> (boolean)
+        // (==, ===, !=, >, <, <=, >=)
+
+        // let age = 18
+        // let result
+
+        // // result = (age == 18)
+        // // result = (age === 18)
+        // // result = (age != 18)
+        // result = (age <= 18)
+
+        // console.log(result);
+
+    // Logical Operators
+        // (&&, ||, !)
+
+        // &&
+
+            // (true)  && (true)  --> true
+            // (true)  && (false) --> false
+            // (false) && (true)  --> false
+            // (false) && (false) --> false
+
+        // ||
+
+            // (true)  || (true)  --> true
+            // (true)  || (false) --> true
+            // (false) || (true)  --> true
+            // (false) || (false) --> false
+
+                // let age = 22
+                // let result
+
+                // // result = (age > 18) && (age > 30)
+                // result = (age > 18) || (age > 30)
+
+                // console.log(!result);
+
+    // Ternary Operator
+
+        // let age = 22
+        // let result
+
+        // result = (age > 18) ? "Eligible for voting" : "Not Eligible"
+
+        // console.log(result);
+        
+    // Type Operator
+
+        // let age = "hello";
+
+        // console.log(age, typeof(age));
+    
