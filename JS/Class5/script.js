c495db4139af7f84fd430ad213c866d7ce32d23f
ManojNambiraj <postboxmano@gmail.com>
2025-09-26 20:38:34 +0530

    JS Arrays & Objects

---
+++ JS/Class5/script.js
@@ -0,0 +1,56 @@
+// Arrays:
+
+    // let nums = [10, 20, 30, 40, 50, 60]
+    // //          0   1    2   3   4  5
+
+    // nums.push()
+    // nums.pop()
+
+    // console.log(nums[1]);
+
+// Objects
+
+    // let student = {
+    //     name: "Raj",
+    //     age: 23,
+    //     mobile: 98765432,
+    //     favColor: ["black", "blue", "pink"],
+    //     address: {
+    //         dno: 2,
+    //         street: "xyz"
+    //     }
+    // }
+
+    // student.address.street = "ABCXYZ"
+
+    // console.log(student.address);
+
+// Destructuring:
+
+    // Array:
+
+        // let fruits = ["apple", "orange", "banana"]
+
+        // const [red, oren, yellow] = fruits
+
+        // console.log(red);
+
+    // Object
+
+        // let student = {
+        //     name: "Raj",
+        //     age: 23,
+        //     mobile: 98765432
+        // }
+
+        // const {name, age, mobile} = student
+
+        // console.log(name);
+        // console.log(age);
+    
+    
+
+    
+
+
+    
